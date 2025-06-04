24b0aacd37572ffedde3fa3ed91a0bf04bf221a6
MrcellSbst <marcellsubastian123@gmail.com>
2025-06-04 17:14:37 +0700

    Big update incoming....
    hehe

---
+++ src/data/imageFiles.js
@@ -0,0 +1,228 @@
+export const mvFiles = [
+    '360°_VR_＂LOVE_TRIP＂.webp',
+    '360°_VR_＂Mae_Shika_Mukanee＂_(Hanya_Lihat_Ke_Depan).webp',
+    'Angin_Sedang_Berhembus.webp',
+    'Beginner.webp',
+    'Belalang_yang_Membangkang_Trainee.webp',
+    'Berdebar_Sisca_.webp',
+    'Bibir_yang_Telah_Dicuri.webp',
+    'Cara_Ceroboh_untuk_Mencinta_(Darashinai_Aishikata).webp',
+    'Cinta_yang_Tulus,_Crescendo_(Junai_no_Crescendo).webp',
+    'Dakishimecha_Ikenai_(Tidak_Boleh_Pelukan).webp',
+    'Dirimu_Melody_(Kimi_wa_Melody).webp',
+    'Everyday,_Kachuusha_(Dance_Version).webp',
+    'Everyday,_Kachuusha_(Story_Version).webp',
+    'Flying_Get.webp',
+    'Flying_High.webp',
+    'Fortune_Cookie_in_Love_(Fortune_Cookie_Yang_Mencinta).webp',
+    'Gingham_Check.webp',
+    'Halloween_Night.webp',
+    'Halloween_Night_(Dangdut_Version).webp',
+    'Heavy_Rotation.webp',
+    'High_Tension.webp',
+    'High_Tension_(Staff_Version).webp',
+    'Indahnya_Senyum_Manismu_dst..webp',
+    'KuSangatSuka.webp',
+    'Laptime_Masa_Remaja_2024_Version.webp',
+    'LOVE_TRIP.webp',
+    'Mae_Shika_Mukanee_(Hanya_Lihat_Ke_Depan).webp',
+    'Magic_Hour.webp',
+    'Malu_Malu_Lollypop.webp',
+    'Manatsu_no_Sounds_Good_(Musim_Panas_Sounds_Good).webp',
+    'Masa_Depan_yang_Menyilaukan_Mata.webp',
+    'Musim_yang_Selanjutnya_(Tsugi_no_Season).webp',
+    'Papan_Penanda_Isi_Hati.webp',
+    'Pareo_adalah_Emerald.webp',
+    'Ponytail_dan_ShuShu.webp',
+    'PunkishV.webp',
+    'Rapsodi.webp',
+    'Refrain_Penuh_Harapan_(Kibouteki_RefrainRefrain_Full_of_Hope).webp',
+    'RIVER.webp',
+    'Saikou_Kayo_(Luar_Biasa).webp',
+    'Sayonara_Crawl.webp',
+    'Sedikit_Saja_I_Love_You.webp',
+    'Sisca_Seharusnya_Kita_Tak_Bersama.webp',
+    'So_Long!.webp',
+    'Sweet_&_Bitter.webp',
+    'UZA.webp',
+    'Value_Milikku_Saja.webp',
+    'Yuuhi_wo_Miteiruka_(Apakah_Kau_Melihat_Mentari_Senja).webp'
+];
+
+export const spvFiles = [
+    'Benang_Sari,_Putik,_dan_KupuKupu_Malam.webp',
+    'Dialog_Dengan_Kenari.webp',
+    'Eureka_Milik_Kita.webp',
+    'GO_AND_FIGHT!_Special_Performance_Video.webp',
+    'Kebun_Binatang_Saat_Hujan.webp',
+    'Langit_Biru_Cinta_Searah.webp',
+    'Only_Today_&_Musim_Panas_yang_Kacau.webp'
+];
+
+export const activeMemberFiles = [
+    'Gen3_feni_fitriyanti.jpg', 'Gen3_shania_gracia.jpg',
+    'Gen6_gita_sekar_andarini.jpg',
+    'Gen7_angelina_christy.jpg', 'Gen7_febriola_sinambela.jpg',
+    'Gen7_freya_jayawardana.jpg', 'Gen7_helisma_putri.jpg',
+    'Gen7_jessica_chandra.jpg', 'Gen7_mutiara_azzahra.jpg',
+    'Gen8_cornelia_vanisa.jpg', 'Gen8_fiony_alveria.jpg',
+    'Gen8_lulu_salsabila.jpg',
+    'Gen9_indah_cahya.jpg', 'Gen9_kathrina_irene.jpg',
+    'Gen9_marsha_lenathea.jpg',
+    'Gen10_amanda_sukma.jpg', 'Gen10_aurellia.jpg',
+    'Gen10_gabriela_abigail.jpg', 'Gen10_indira_seruni.jpg',
+    'Gen10_jesslyn_elly.jpg', 'Gen10_raisha_syifa.jpg',
+    'Gen11_alya_amanda.jpg', 'Gen11_anindya_ramadhani.jpg',
+    'Gen11_cathleen_nixie.jpg', 'Gen11_celline_thefani.jpg',
+    'Gen11_chelsea_davina.jpg', 'Gen11_cynthia_yaputera.jpg',
+    'Gen11_dena_natalia.jpg', 'Gen11_desy_natalia.jpg',
+    'Gen11_gendis_mayrannisa.jpg', 'Gen11_grace_octaviani.jpg',
+    'Gen11_greesella_adhalia.jpg', 'Gen11_michelle_alexandra.jpg',
+    'Gen12_abigail_rachel.jpg', 'Gen12_adeline_wijaya.jpg',
+    'Gen12_aurhel_alana.jpg', 'Gen12_catherina_vallencia.jpg',
+    'Gen12_fritzy_rosmerian.jpg', 'Gen12_hillary_abigail.jpg',
+    'Gen12_jazzlyn_trisha.jpg', 'Gen12_michelle_levia.jpg',
+    'Gen12_nayla_suji.jpg', 'Gen12_nina_tutachia.jpg',
+    'Gen12_oline_manuel.jpg', 'Gen12_regina_wilian.jpg',
+    'Gen12_ribka_budiman.jpg', 'Gen12_shabilqis_naila.jpg',
+    'Gen12_victoria_kimberly.jpg',
+    'Gen13_Astrella_Virgiananda.jpg', 'Gen13_Aulia_Riza.jpg',
+    'Gen13_Bong_Aprilli.jpg', 'Gen13_Hagia_Sopia.jpg',
+    'Gen13_Humaira_Ramadhani.jpg', 'Gen13_Jacqueline_Immanuela.jpg',
+    'Gen13_Jemima_Evodie.jpg', 'Gen13_Mikaela_Kusjanto.jpg',
+    'Gen13_Nur_Intan.jpg', 'JKT48V_Gen1_Pia_Meraleo.webp', 'JKT48V_Gen1_Tana_Nona.webp'
+];
+
+export const exMemberFiles = [
+    // Gen1 members
+    'Gen1/Gen1_aki_takajo.jpg', 'Gen1/Gen1_alissa_galliamova.webp', 'Gen1/Gen1_allisa_astri.webp',
+    'Gen1/Gen1_ayana_shahab.webp', 'Gen1/Gen1_beby_chaesara_anadila.webp', 'Gen1/Gen1_cindy_gulla.webp',
+    'Gen1/Gen1_cleopatra.webp', 'Gen1/Gen1_delima_rizky.webp', 'Gen1/Gen1_devi_kinal_putri.webp',
+    'Gen1/Gen1_diasta_priswarini.webp', 'Gen1/Gen1_fahira.webp', 'Gen1/Gen1_frieska_anastasia_laksani.webp',
+    'Gen1/Gen1_gabriella.jpg', 'Gen1/Gen1_ghaida_farisya.webp', 'Gen1/Gen1_haruka_nakagawa.jpg',
+    'Gen1/Gen1_intania_pratama_ilham.webp', 'Gen1/Gen1_jessica_vania.webp', 'Gen1/Gen1_jessica_veranda.jpg',
+    'Gen1/Gen1_melody_nurramdhani_laksani.webp', 'Gen1/Gen1_nabilah_ratna_ayu_azalia.jpg', 'Gen1/Gen1_neneng_rosediana.webp', 'Gen1/Gen1_rena_nozawa.webp',
+    'Gen1/Gen1_rezky_wiranti_dhike.webp', 'Gen1/Gen1_rica_leyona.webp', 'Gen1/Gen1_sendy_ariani.webp',
+    'Gen1/Gen1_shania_junianatha.webp', 'Gen1/Gen1_siti_gayatri.webp', 'Gen1/Gen1_sonia_natalia.webp',
+    'Gen1/Gen1_sonya_pandarmawan.webp', 'Gen1/Gen1_stella_cornelia.webp',
+
+    // Gen2 members
+    'Gen2/Gen2_alicia_chanzia.webp', 'Gen2/Gen2_althea_callista.webp', 'Gen2/Gen2_annisa_athia.webp',
+    'Gen2/Gen2_rina_chikano.jpg', 'Gen2/Gen2_cindy_yuvia.webp', 'Gen2/Gen2_della_delila.webp',
+    'Gen2/Gen2_dellia_erdita.webp', 'Gen2/Gen2_dena_siti_rohyati.webp', 'Gen2/Gen2_dwi_putri_bonita.webp',
+    'Gen2/Gen2_fakhriyani_shafariyanti.webp', 'Gen2/Gen2_intar_putri_kariina.webp', 'Gen2/Gen2_jennifer_hanna.webp',
+    'Gen2/Gen2_jennifer_rachel_natasya.webp', 'Gen2/Gen2_lidya_maulida_djuhandar.webp', 'Gen2/Gen2_nadhifa_karimah.webp',
+    'Gen2/Gen2_nadila_cindi_wantari.webp', 'Gen2/Gen2_natalia.webp', 'Gen2/Gen2_noella_sisterina.webp',
+    'Gen2/Gen2_novinta_dhini.webp', 'Gen2/Gen2_nurhalima_oktavianti.webp', 'Gen2/Gen2_octi_sevpin.webp',
+    'Gen2/Gen2_olivia_robberecht.webp', 'Gen2/Gen2_priscillia_sari_dewi.webp', 'Gen2/Gen2_ratu_vienny_fitrilya.webp',
+    'Gen2/Gen2_riskha_fairunissa.webp', 'Gen2/Gen2_rona_anggreani.webp', 'Gen2/Gen2_saktia_oktapyani.webp',
+    'Gen2/Gen2_shinta_naomi.webp', 'Gen2/Gen2_sinka_juliani.webp', 'Gen2/Gen2_thalia.webp',
+    'Gen2/Gen2_thalia_ivanka_elizabeth.webp', 'Gen2/Gen2_viviyona_apriani.webp',
+
+    // Gen3 members
+    'Gen3/Gen3_alycia_ferryana.webp', 'Gen3/Gen3_amanda_dwi_arista.webp', 'Gen3/Gen3_andela_yuwono.webp',
+    'Gen3/Gen3_anggie_putri_kurniasari.webp', 'Gen3/Gen3_aninditha_rahma_cahyadi.webp', 'Gen3/Gen3_ayu_safira_oktaviani.webp',
+    'Gen3/Gen3_chikita_ravenska_mamesah.webp', 'Gen3/Gen3_elaine_hartanto.webp', 'Gen3/Gen3_farina_yogi_devani.webp',
+    'Gen3/Gen3_fransisca_saraswati_puspa_dewi.webp', 'Gen3/Gen3_indah_permata_sari.webp', 'Gen3/Gen3_kezia_putri_andinta.webp',
+    'Gen3/Gen3_maria_genoveva_natalia_desy_purnamasari_gunawan.webp', 'Gen3/Gen3_martha_graciela.webp',
+    'Gen3/Gen3_michelle_christo_kusnadi.webp', 'Gen3/Gen3_milenia_christien_glory_goenawan.webp',
+    'Gen3/Gen3_nadhifa_salsabila.webp', 'Gen3/Gen3_nina_hamidah.webp', 'Gen3/Gen3_ni_made_ayu_vania_aurellia.webp',
+    'Gen3/Gen3_pipit_ananda.webp', 'Gen3/Gen3_putri_farin_kartika.webp', 'Gen3/Gen3_rizka_khalila.webp',
+    'Gen3/Gen3_shaffa_nabila.webp', 'Gen3/Gen3_shani_indira_natio.webp', 'Gen3/Gen3_sofia_meifaliani.webp',
+    'Gen3/Gen3_stephanie_pricilla_indarto_putri.webp', 'Gen3/Gen3_syahfira_angela_nurhaliza.webp',
+    'Gen3/Gen3_triarona_kusuma.webp', 'Gen3/Gen3_yansen_indiani.webp', 'Gen3/Gen3_zebi_magnolia_fawwaz.webp',
+
+    // Gen4 members
+    'Gen4/Gen4_adriani_elisabeth.webp', 'Gen4/Gen4_christi.webp', 'Gen4/Gen4_cindy_hapsari.webp',
+    'Gen4/Gen4_fidly_immanda.webp', 'Gen4/Gen4_jessica_berliana.webp', 'Gen4/Gen4_jinan_safa_safira.webp',
+    'Gen4/Gen4_made_devi.webp', 'Gen4/Gen4_mega_suryani.webp', 'Gen4/Gen4_melati_putri.webp',
+    'Gen4/Gen4_sri_lintang.webp', 'Gen4/Gen4_tan_zhi_hui_celine.jpg',
+    'Gen4/Gen4_zahra_yuriva.webp',
+
+    // Gen5 members
+    'Gen5/Gen5_adhisty_zara.webp', 'Gen5/Gen5_anggita_destiana.webp', 'Gen5/Gen5_chintya_hanindhitakirana.webp',
+    'Gen5/Gen5_citra_ayu.webp', 'Gen5/Gen5_diani_amalia.webp', 'Gen5/Gen5_elizabeth_gloria.webp',
+    'Gen5/Gen5_eve_antoinette.webp', 'Gen5/Gen5_gabryela_marcelina.webp', 'Gen5/Gen5_hasyakyla_utami.webp',
+    'Gen5/Gen5_helma_sonya.webp', 'Gen5/Gen5_nurhayati.webp', 'Gen5/Gen5_puti_nadhira.webp',
+    'Gen5/Gen5_regina_angelina.webp', 'Gen5/Gen5_rissanda_putri.webp', 'Gen5/Gen5_ruth_damayanti.webp',
+    'Gen5/Gen5_sania_julia.webp', 'Gen5/Gen5_violeta_burhan.webp',
+
+    // Gen6 members
+    'Gen6/Gen6_amanda_priscella.webp', 'Gen6/Gen6_anastasya_narwastu.webp', 'Gen6/Gen6_ariella_calista.webp',
+    'Gen6/Gen6_denise_caroline.webp', 'Gen6/Gen6_erika_ebisawa.webp', 'Gen6/Gen6_erika_sintia.webp',
+    'Gen6/Gen6_graciella_ruth.webp', 'Gen6/Gen6_jihan_miftahul.webp', 'Gen6/Gen6_kandiya_rafa.webp', 'Gen6/Gen6_Saya_Kawamoto.webp',
+    'Gen6/Gen6_putri_cahyaning.webp', 'Gen6/Gen6_rinanda_syahputri.webp', 'Gen6/Gen6_riska_amelia.webp',
+    'Gen6/Gen6_shalza_grasita.webp',
+
+    // Gen7 members
+    'Gen7/Gen7_aiko_harumi.webp', 'Gen7/Gen7_aurel_mayori.webp', 'Gen7/Gen7_azizi_asadel.webp',
+    'Gen7/Gen7_calista_lea.webp', 'Gen7/Gen7_dhea_angelia.webp', 'Gen7/Gen7_febi_komaril.webp',
+    'Gen7/Gen7_febrina_diponegoro.webp', 'Gen7/Gen7_gabriel_angelina.webp', 'Gen7/Gen7_jesslyn_callista.webp',
+    'Gen7/Gen7_kanya_caya.webp', 'Gen7/Gen7_nabila_fitriana.webp', 'Gen7/Gen7_rifa_fatmasari.webp',
+    'Gen7/Gen7_viona_fadrin.webp', 'Gen7/Gen7_yessica_tamara.webp',
+
+    // Gen8 members
+    'Gen8/Gen8_amanina_afiqah.webp', 'Gen8/Gen8_amirah_fatin.webp', 'Gen8/Gen8_cindy_nugroho.webp',
+    'Gen8/Gen8_devytha_maharani.webp', 'Gen8/Gen8_eriena_kartika.webp', 'Gen8/Gen8_flora_shafiq.webp',
+    'Gen8/Gen8_gabriella_stevany.webp', 'Gen8/Gen8_keisya_ramadhani.webp', 'Gen8/Gen8_nyimas_ratu_rafa.webp',
+    'Gen8/Gen8_pamela_krysanthe.webp', 'Gen8/Gen8_reva_adriana.webp', 'Gen8/Gen8_reva_fidela.webp',
+    'Gen8/Gen8_salma_annisa.webp', 'Gen8/Gen8_umega_maulana.webp', 'Gen8/Gen8_zahra_nur.webp',
+
+    // Gen9 members
+    'Gen9/Gen9_adzana_shaliha.webp', 'Gen9/Gen9_caithlyn_gwyneth.webp', 'Gen9/Gen9_chalista_ellysia.webp',
+    'Gen9/Gen9_christabel_jocelyn.webp', 'Gen9/Gen9_iris_vevina_prasetio.webp', 'Gen9/Gen9_nabila_gusmarlia.webp',
+    'Gen9/Gen9_olivia_payten.webp', 'Gen9/Gen9_putri_elzahra.webp', 'Gen9/Gen9_shinta_devi.webp',
+    'Gen9/Gen9_tiara_sasi.webp',
+
+    // Gen10 members
+    'Gen10/Gen10_abieza_syabira.webp', 'Gen10/Gen10_alia_giselle.jpg', 'Gen10/Gen10_callista_alifia.webp',
+    'Gen10/Gen10_danessa_valerie.webp', 'Gen10/Gen10_naura_safinatunnajah.webp',
+
+    // Gen11 members
+    'Gen11/Gen11_aulia_asyira.webp', 'Gen11/Gen11_jeane_victoria.webp',
+
+    // Gen12 members
+    'Gen12/Gen12_aisa_maharani.webp', 'Gen12/Gen12_letycia_moreen.webp',
+
+    // JKT48V members
+    'JKT48V_GEN1/JKT48V_Gen1_Kanaia_Asa.webp'
+];
+
+export const setlistFiles = [
+    'Aturan_Anti_Cinta.jpg',
+    'Banzai.jpg',
+    'Bel_Terakhir_Berbunyi.jpg',
+    'BELIEVE.jpg',
+    'Boku_No_Taiyou.jpg',
+    'Cara_Meminum_Ramune.jpg',
+    'Demi_Seseorang.jpg',
+    'Dewi_Theater.jpg',
+    'Fajar_Sang_Idola.jpg',
+    'Fly!_Team_T.jpg',
+    'Gadis_Gadis_Remaja.jpg',
+    'Himawarigumi.jpg',
+    'Ingin_Bertemu.webp',
+    'Pajama_Drive.jpg',
+    'Saka_Agari.jpg',
+    'Romansa_Sang_Gadis.jpg',
+    'Sambil_Menggandeng_Erat_Tanganku.jpg',
+    'Sekarang_Sedang_Jatuh_Cinta.jpg',
+    'Tunas_di_Balik_Seragam.jpg'
+];
+
+export const ssRamadanFiles = [
+    'Anak_King.jpg',
+    'Catur_Asta.jpg',
+    'Cosmostales.jpg',
+    'Dancing_Queen.jpg',
+    'Family48.jpg',
+    'Host_Sweet_Host.jpg',
+    'Jika_Aku_Menjadi.jpg',
+    'JKT48_School.jpg',
+    'Quit_Game.jpg',
+    'Realm_Rumble.jpg',
+    'Seleksi_PMDK.jpg',
+    'The_Quiz_Show.jpg',
+    'The_Slap_Show.jpg',
+    'Vocal_Queen.jpg'
+];
