b2ab9046d836088f2bbebcfe441601267677fdc5
MrcellSbst <105704078+MrcellSbst@users.noreply.github.com>
2026-04-26 15:08:38 +0700

    moving to cloudflare

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Box } from '@mui/material'
 import Homepage, { HomepageTierlist, HomepageTools, HomepageGames } from './Homepage'
 import TierlistCombined from './Tierlist_Combined'
@@ -69,8 +67,6 @@ function App() {
         </Box>
         <Footer />
       </Box>
-      <Analytics />
-      <SpeedInsights />
     </Router>
   )
 }
