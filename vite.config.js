4ea6ba5361a0aaebc925f5e429a42c1e99b79f44
MrcellSbst <marcellsubastian123@gmail.com>
2026-04-26 23:08:14 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+  
+
+
+
 export default defineConfig({
+	preview: {
+    allowedHosts: ['v1.tierlistjkt48.my.id', 'v3.tierlistjkt48.my.id']
+  },
+	
+	
+	
     plugins: [
         react(),
         VitePWA({
